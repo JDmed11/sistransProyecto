0f041c455a41ec6e28d90fac97898b0397d94133
32DANIEL <d.rudas@uniandes.edu.co>
2023-11-06 14:07:03 -0500

    RF4 y RF6

--- backend/src/main/java/uniandes/edu/co/proyecto/controller/ServiciosController.java
+++ backend/src/main/java/uniandes/edu/co/proyecto/controller/ServiciosController.java
@@ -1,9 +1,12 @@
 package uniandes.edu.co.proyecto.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import uniandes.edu.co.proyecto.repository.ServicioRepository;
 
@@ -14,8 +17,23 @@ public class ServiciosController {
     private ServicioRepository servicioRepository;
 
     @GetMapping("/servicios")
-    public String getAll(Model model){
-        model.addAttribute("servicios", servicioRepository.getAll());
+    public String getFilteredServices(Model model,
+                                      @RequestParam(required = false) Double precioMin,
+                                      @RequestParam(required = false) Double precioMax,
+                                      @RequestParam(required = false) String fechaInicio,
+                                      @RequestParam(required = false) String fechaFin,
+                                      @RequestParam(required = false) Long empleadoId,
+                                      @RequestParam(required = false) Long tipoServicioId,
+                                      @RequestParam(required = false) Long categoriaServicioId) {
+        LocalDate inicio = fechaInicio != null ? LocalDate.parse(fechaInicio) : null;
+        LocalDate fin = fechaFin != null ? LocalDate.parse(fechaFin) : null;
+        // Llamar al método del repo
+        List<Servicio> servicios = servicioRepository.findWithFilters(
+                precioMin, precioMax, inicio, fin, empleadoId, tipoServicioId, categoriaServicioId);
+        
+        // Agregar servicios filtrados al modelo
+        model.addAttribute("servicios", servicios);
+        
         return "Servicios/lista";
     }
 }
